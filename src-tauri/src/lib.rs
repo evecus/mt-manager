8a41bf9523a30429fb9d8748017c39e92447f75e
evecus <jamie55@163.com>
2026-06-29 19:44:58 +0800

    Add files via upload

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -627,7 +627,6 @@ pub fn get_parent_path(path: String) -> Option<String> {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
